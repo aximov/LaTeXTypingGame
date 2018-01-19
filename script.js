5df811eff30728a40bfd223934d09fa476490ef8
Akimoto Daichi <deltalphan+github@gmail.com>
2018-01-20 02:23:33 +0900

    ランダムに問題を選択

--- script.js
+++ script.js
@@ -3,14 +3,17 @@
 
   console.log('Wow wow wow');
 
-  const arr = ['\\(\\pi\\)'];
+  const arr = ['\\(\\pi\\)', '\\(\\frac{8\\pi h\\nu^3}{c^3}\\frac{1}{\\exp\\left(\\frac{h\\nu}{k_{\\mathrm{B}}T}\\right)-1}\\)', '\\(\\xi\\)', '\\(\\frac{1}{\\sqrt{2\\pi\\sigma^2}}\\exp\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)\\)'];
   const game = {
     startTime: null,
     result: document.getElementById('result'),
     ans: document.getElementById('answer'),
     prob: document.getElementById('problem'),
-    expr: arr[0],
+    rand: null,
+    expr: null,
     start: function () {
+      game.rand = Math.floor(Math.random()*arr.length);
+      game.expr = arr[game.rand];
       game.startTime = new Date().getTime();
       game.prob.innerText = game.expr;
       game.ans.onkeyup = game.judge;
