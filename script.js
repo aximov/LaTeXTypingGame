2becb385c846b08999d91a1cd1c08b2588c35ebc
Akimoto Daichi <deltalphan+github@gmail.com>
2018-01-20 21:41:58 +0900

    now example answers appear

--- script.js
+++ script.js
@@ -3,24 +3,26 @@
 
   console.log('Wow wow wow');
 
-  const arr = ['\\(\\pi\\)', '\\(\\frac{8\\pi h\\nu^3}{c^3}\\frac{1}{\\exp\\left(\\frac{h\\nu}{k_{\\mathrm{B}}T}\\right)-1}\\)', '\\(\\xi\\)', '\\(\\frac{1}{\\sqrt{2\\pi\\sigma^2}}\\exp\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)\\)', '\\(\\alpha\\)', '\\(\\beta\\)', '\\(\\varepsilon\\)', '\\(\\Gamma\\)', '\\(\\rho\\)', '\\(\\epsilon\\)', '\\(n(r)=n_0\\left[1+\\left(\\frac{r}{r_c}\\right)^2\\right]^{-3/2}\\)'];
+  const arr = ['\\pi', '\\frac{8\\pi h\\nu^3}{c^3}\\frac{1}{\\exp\\left(\\frac{h\\nu}{k_{\\mathrm{B}}T}\\right)-1}', '\\xi', '\\frac{1}{\\sqrt{2\\pi\\sigma^2}}\\exp\\left(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\right)', '\\alpha', '\\beta', '\\varepsilon', '\\Gamma', '\\rho', '\\epsilon', 'n(r)=n_0\\left[1+\\left(\\frac{r}{r_c}\\right)^2\\right]^{-3/2}'];
   const game = {
     startTime: null,
     result: document.getElementById('result'),
     ans: document.getElementById('answer'),
     prob: document.getElementById('problem'),
+    ex: document.getElementById('example'),
     rand: null,
     expr: null,
     start: function () {
       game.rand = Math.floor(Math.random()*arr.length);
       game.expr = arr[game.rand];
       game.startTime = new Date().getTime();
-      game.prob.innerText = game.expr;
+      game.prob.innerText = '\\(' + game.expr + '\\)';
+      game.ex.innerText = game.expr;
       game.ans.onkeyup = game.judge;
     },
     judge: function () {
       console.log('ye');
-      if ('\\(' + game.ans.value + '\\)' === game.expr){
+      if (game.ans.value === game.expr){
         game.stop();
       }
     },
